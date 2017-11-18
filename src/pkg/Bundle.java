0af03eeceec2e3e3ad214e558890605a86578e3c
Connor <cbyrd@mail.umw.edu>
2017-11-18 15:58:51 -0500

    bundle

--- src/pkg/Bundle.java
+++ src/pkg/Bundle.java
@@ -2,10 +2,27 @@ package pkg;
 
 class Bundle {
 	private String bundleName;
-	ItemList items;
+	private ItemList items;
 
-	public Bundle(String name,) {
+	public String getBundleName() {
+		return bundleName;
+	}
 
+	public void setBundleName(String bundleName) {
+		this.bundleName = bundleName;
+	}
+
+	public ItemList getItems() {
+		return items;
+	}
+
+	public void setItems(ItemList items) {
+		this.items = items;
+	}
+
+	public Bundle(String name,ItemList list) {
+		this.bundleName.equals(name);
+		this.items=list;
 	}
 
 }
