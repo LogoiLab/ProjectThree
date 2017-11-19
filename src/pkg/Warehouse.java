f441574d6d99a4a50bcdf0739d4721a6bc934c99
Connor <cbyrd@mail.umw.edu>
2017-11-18 22:20:03 -0500

    warehouse and mainwarehouse

--- src/pkg/Warehouse.java
+++ src/pkg/Warehouse.java
@@ -4,26 +4,25 @@ import java.util.ArrayList;
 import java.util.HashMap;
 
 public class Warehouse {
-    private String warehouseListName;
     private String name;
     private ArrayList<ItemList> partList;
     private HashMap<String, Warehouse> warehouseList = new HashMap<>();
 
     public String getWarehouseListName() {
-        return warehouseListName;
+        return name;
     }
 
     public void setWarehouseListName(String warehouseListName) {
-        this.warehouseListName = warehouseListName;
+        this.name = warehouseListName;
     }
     
     public Warehouse(String whName, ArrayList<ItemList> partList) {
-        this.warehouseListName = whName;
+        this.name = whName;
         this.partList = partList;
     }
+
+    public Warehouse(){}
     
-    //Double check and see if this is what we want or am I being stupid!
-    //Thouhgt process if warehouseName is in the warehouse list return that warehouses itemlist
      public ArrayList<ItemList> getWarehouseList(String warehouseName){
         for(int i=0;i<warehouseList.size();i++){
             if(warehouseName.equals(warehouseList.get(i).getName())){
