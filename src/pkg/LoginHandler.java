f80f5f1ea3a07aebc13904325fc568892ad7f5d5
Chad Baxter <cbaxter@mail.umw.edu>
2017-11-18 20:52:52 -0500

    login handler impl

--- src/pkg/LoginHandler.java
+++ src/pkg/LoginHandler.java
@@ -1,12 +1,27 @@
 package pkg;
 
+import java.util.HashMap;
+
 public class LoginHandler{
 	private static final LoginHandler instance = new LoginHandler();
+	private HashMap<String, Account> accountList = new HashMap<String, Account>();
 
 	//private constructor to avoid client applications to use constructor
 	private LoginHandler(){}
 
-	public static LoginHandler getInstance(){
+	public static synchronized LoginHandler getInstance() {
 		return instance;
 	}
+
+	public boolean doLogin(String uname, String upass) {
+    return accountList.get(uname).checkHash(uname, upass);
+  }
+
+  public void addAccount(Account account) {
+	  accountList.put(account.getUserName(), account);
+  }
+
+  public Account getAccount(String uname) {
+    return accountList.get(uname);
+  }
 }
