17cd103af573d28a98b2c5f89d28cbfdae49b1b9
Chad Baxter <cbaxter@mail.umw.edu>
2017-11-18 23:39:31 -0500

    Invoice work

--- src/pkg/InvoiceFactory.java
+++ src/pkg/InvoiceFactory.java
@@ -14,8 +14,12 @@ public class InvoiceFactory {
   }
 
   public SalesInvoice createInvoice(Account account, String customer, ArrayList<String> invoiceParts) {
-    ArrayList<Item> currList = new ArrayList<Item>();
-    currList.add(new Item(WarehouseFactory.getInstance().getWarehouse("MainWarehouse")))
+    ArrayList<BikePart> currList = new ArrayList<BikePart>();
+
+    for (String temp: invoiceParts) {
+      BikePart part = WarehouseFactory.getInstance().getWarehouse("MainWarehouse").getItemList().getPartByNumber(temp.split(";")[0]);
+      currList.add(part);
+    }
     return new SalesInvoice(account, customer, new ItemList(currList));
   }
 }
