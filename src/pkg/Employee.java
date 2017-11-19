4a3e5b9029f09e6ed350e6379cad32295b75cc26
Chad Baxter <cbaxter@mail.umw.edu>
2017-11-19 03:07:12 -0500

    added commission field to employee

--- src/pkg/Employee.java
+++ src/pkg/Employee.java
@@ -3,11 +3,20 @@ package pkg;
 class Employee extends Account {
 
   private static int PERM_LEVEL = 1;
+  private static double com = 0.15;
 
   public Employee(String userName, String password) {
     super.userName = userName;
     super.passSalt = Hasher.generateSalt();
     super.passHash = Hasher.hashPassword(password.toCharArray(), super.passSalt, 32, 512);
+  }
+
+  public double getCommission() {
+    return this.com;
+  }
+
+  public void setCommission(double com) {
+    this.com = com;
   }
 
 }
